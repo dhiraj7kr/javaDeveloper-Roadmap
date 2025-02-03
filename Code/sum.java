190d3d3463bc6087132e88ea5fb79fa2fe612865
DHIRAJ KUMAR <110246188+dhiraj7kr@users.noreply.github.com>
2025-02-03 12:01:06 +0530

    Create sum.java

---
+++ Code/sum.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+class Sum {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter two number");
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int sum =a +b;
+        
+        System.out.println("Sum of two number is "+sum);
+    }
+}
