b0514971aeaac1fd2dfab2642fb65632e60ebc44
DHIRAJ KUMAR <110246188+dhiraj7kr@users.noreply.github.com>
2025-02-03 17:50:16 +0530

    Create Inheritanse.java

---
+++ Code/Inheritanse.java
@@ -0,0 +1,26 @@
+// Inhertense
+
+import java.util.*;
+class Book{
+    void read(){
+        System.out.println("Today I am reading the book ");
+    }
+}
+class Library extends Book{
+    void borrowed(){
+        System.out.println("That I borrowed from Library");
+    }
+}
+// main class
+public class Main{
+    public static void main(String[] args){
+        Library mybook= new Library();
+        mybook.read();
+        mybook.borrowed();
+    }
+}
+
+//output:
+//------------------------
+// Today I am reading the book 
+// That I borrowed from Library
